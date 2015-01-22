a9cef24e75a7a6a1b5bf839b1b7e4a6a20140dd0
Maxime Tary <maxime.tary@quadra-informatique.fr>
2015-01-22 15:28:31 +0000

    ajout traduction

---
+++ translations/fr.php
@@ -0,0 +1,17 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_bf2600eeb48a3c674ecd0424a075a1ae'] = 'Export simple en csv des commandes';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_7b75a835afecf4c5ef14dc1c0f49831e'] = 'Ce module exporte en csv les commandes';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_5992528f236587164ee4f212b9460410'] = 'Données invalides';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_c888438d14855d7d96a2724ee9c306bd'] = 'Configuration sauvegardée';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_862c9aa04b6c7968b40150ad81c8e880'] = 'Paramètres d\'export';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_944135185c23a059110cdf627c470521'] = 'Envoyer une copie par Email';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_93cba07454f06a4a960172bbd6e2a435'] = 'Oui';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_bafd7322c6e97d25b6299b5d6fe8920b'] = 'Non';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_ce8ae9da5b7cd6c3df2929543a9af92d'] = 'Email';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_cc5cf1e94fb2119f90c3de38d2d3ba44'] = 'Exporter cette commande par mail';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_8cf2241d54ba9e0f7cfe33463d40b53c'] = 'La commande a été exportée avec succès.';
+$_MODULE['<{simplecsvexport}prestashop>simplecsvexport_5cb093947eec0af03a5e480fe1fe0a1d'] = 'CSV export : Commande ';
